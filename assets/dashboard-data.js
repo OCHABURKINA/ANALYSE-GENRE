ac7665673143976d5275d8523e97eb79928cdbf0
OCHABURKINA <hypolite.reoutarmbailassem1@un.org>
2026-06-09 19:48:27 +0000

    Create dashboard-data.js

---
+++ assets/dashboard-data.js
@@ -0,0 +1,55 @@
+const DASHBOARD_DATA = {
+  regions: [
+    {
+      id: "bankui",
+      name: "Boucle du Mouhoun (Bankui)",
+      total: 0,
+      adults: 0,
+      adolescents: 0
+    },
+    {
+      id: "goulmou",
+      name: "Est (Goulmou)",
+      total: 0,
+      adults: 0,
+      adolescents: 0
+    },
+    {
+      id: "kadiogo",
+      name: "Centre (Kadiogo)",
+      total: 0,
+      adults: 0,
+      adolescents: 0
+    },
+    {
+      id: "koulse",
+      name: "Centre-Nord (Koulsé)",
+      total: 0,
+      adults: 0,
+      adolescents: 0
+    },
+    {
+      id: "liptako",
+      name: "Sahel (Liptako)",
+      total: 0,
+      adults: 0,
+      adolescents: 0
+    },
+    {
+      id: "nakambe",
+      name: "Centre-Sud (Nakambé)",
+      total: 0,
+      adults: 0,
+      adolescents: 0
+    },
+    {
+      id: "yaadga",
+      name: "Nord (Yaadga)",
+      total: 0,
+      adults: 0,
+      adolescents: 0
+    }
+  ]
+};
+
+window.DASHBOARD_DATA = DASHBOARD_DATA;
